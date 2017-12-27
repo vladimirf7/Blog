edd6e116144c9c288afe91568a0da47d011f6671
VF <vf@example.com>
2017-12-27 11:28:27 -0500

    'WIP'

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const config = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 const app = express();
 
 // mongoose.connect('mongodb://localhost/edu_blog');
@@ -9,8 +10,20 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now },
+});
+const Blog = mongoose.model('Blog', blogSchema);
+
 app.get('/', (req, res) => {
-  res.send('Something will be here...');
+  res.redirect('/blogs');
+});
+
+app.get('/blogs', (req, res) => {
+  res.render('index');
 });
 
 app.listen(config.PORT, () => {
---
+++ test/app_test.js
@@ -0,0 +1,9 @@
+/* eslint no-undef: 0 */
+
+const assert = require('assert');
+
+describe('Test the tests', () => {
+  it('should run some tests', () => {
+    assert.equal(1, 1);
+  });
+});
