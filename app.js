75648b30ea826bfb4170cef31459d63d2b2bc755
VF <vf@example.com>
2017-12-28 21:12:08 -0500

    Add EDIT

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const config = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost/edu_blog');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -54,6 +56,27 @@ app.get('/blogs/:id', (req, res) => {
       res.render('show', { blog: foundBlog });
     }
   });
+});
+
+app.get('/blogs/:id/edit', (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.render('edit', { blog: foundBlog });
+    }
+  });
+});
+
+app.put('/blogs/:id', (req, res) => {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, foundBlog) => {
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.redirect(`/blogs/${req.params.id}`);
+    }
+  });
+  // res.send('here');
 });
 
 app.listen(config.PORT, () => {
