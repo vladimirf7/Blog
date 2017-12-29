49fb9574005ae5a37854a20adcfed88df48a289f
VF <vf@example.com>
2017-12-28 20:31:06 -0500

    - Add blog done - Fix CSS issue

--- app.js
+++ app.js
@@ -22,6 +22,20 @@ app.get('/', (req, res) => {
   res.redirect('/blogs');
 });
 
+app.get('/blogs/new', (req, res) => {
+  res.render('new');
+});
+
+app.post('/blogs', (req, res) => {
+  Blog.create(req.body.blog, (err, newBlog) => {
+    if (err) {
+      res.render('new');
+    } else {
+      res.redirect('/blogs');
+    }
+  });
+});
+
 app.get('/blogs', (req, res) => {
   Blog.find({}, (err, blogs) => {
     if (err) {
